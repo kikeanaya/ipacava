94c14337342d4fbdb5d18a7dc3c5c4c6cb712676
kikeanaya <luiseanayam@gmail.com>
2019-02-20 22:21:46 +0100

    changed font style

--- ipaCava/js/ui.js
+++ ipaCava/js/ui.js
@@ -15,7 +15,7 @@ Ui.prototype.drawDepth = function (){
     this.game.ctx.fillStyle = "grey"
     this.game.ctx.fillRect(1255, 27, 150, 29)
 
-    this.game.ctx.font = "25px racing sans one";
+    this.game.ctx.font = "25px Love Ya Like A Sister";
     this.game.ctx.fillStyle = "white";
     this.game.ctx.fillText("DEPTH: " +this.game.depth + "m", 1262, 50);
 
@@ -25,31 +25,34 @@ Ui.prototype.drawPlayerHealth = function (){
     this.game.ctx.fillStyle = "grey"
     this.game.ctx.fillRect(20, 27, 330, 29)
 
-    this.game.ctx.font = "25px racing sans one";
+    this.game.ctx.font = "25px Love Ya Like A Sister";
     this.game.ctx.fillStyle = "white";
     this.game.ctx.fillText("HEALTH: " + this.game.player.health, 30, 50);
 }
 
 Ui.prototype.drawMoney = function (){
-    this.game.ctx.font = "25px racing sans one";
+    this.game.ctx.font = "25px Love Ya Like A Sister";
     this.game.ctx.fillStyle = "white";
     this.game.ctx.fillText("MONEY: " + this.game.money, 200, 50);
 }
 
 Ui.prototype.drawShop = function (){
     this.game.ctx.fillStyle = "grey"
-    this.game.ctx.fillRect(500, 230, 500, 300)
+    this.game.ctx.fillRect(500, 210, 500, 350)
 
-    this.game.ctx.font = "25px racing sans one";
+    this.game.ctx.font = "30px Love Ya Like A Sister";
     this.game.ctx.fillStyle = "white";
-    this.game.ctx.fillText("BUY: WEAPON1", 550, 270);
+    this.game.ctx.fillText("PRESS 1 TO BUY:", 540, 270);
     
     this.game.ctx.drawImage(this.weapon1, 532, 285, this.weaponsWidth, this.weaponsHeight)
 
-    this.game.ctx.font = "25px racing sans one";
+    this.game.ctx.font = "30px Love Ya Like A Sister";
     this.game.ctx.fillStyle = "white";
-    this.game.ctx.fillText("BUY: WEAPON2", 780, 270);
+    this.game.ctx.fillText("PRESS 2 TO BUY:", 770, 270);
 
     this.game.ctx.drawImage(this.weapon2, 764, 285, this.weaponsWidth, this.weaponsHeight)
 
+    this.game.ctx.font = "25px Love Ya Like A Sister";
+    this.game.ctx.fillStyle = "white";
+    this.game.ctx.fillText("PRESS 'S' TO EXIT SHOP", 640, 530);
 }
